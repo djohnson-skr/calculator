2ace698aa3688f71df6660cb9894176040f2cabd
djohnson-skr <arminefendic12@gmail.com>
2021-09-15 23:16:17 -0400

    Major bugs fixed and code commented

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* --- GLOBAL VARIABLES --- */
 let tempZero = "0";
 let tempVal = "";
 let val2 = "";
@@ -6,8 +7,8 @@ let operation = "";
 let result = "";
 let precision = 10; 
 let numberSize = 8;
-let newDisplayValue = "";
 
+/* --- DOM OBJECTS --- */
 const buttons = document.querySelectorAll(".calc-buttons button");
 const display = document.getElementById("display"); 
 const operators = document.querySelectorAll(".operator");
@@ -19,6 +20,7 @@ display.textContent = tempZero; // set initial value on display
 
 buttons.forEach(button => {
     button.addEventListener("click", function(e) {
+        // ensure that only 1 decimal can be used
         if (button.textContent == ".") {
             if (!/\./.test(tempVal)) {
                 tempVal += ".";
@@ -26,31 +28,48 @@ buttons.forEach(button => {
             };
         } 
         else {
-            tempVal += button.textContent;
-            tempVal = tempVal.substring(0,numberSize);
-            display.textContent = tempVal;
-            if (tempVal.length == numberSize) {
-                tempVal = tempVal + "1";
+            // functionality delete button when a result is already calculated
+            if (result != "" && button.textContent == "Delete") {
+                result = result.toString();
+                result = result.substr(0, result.length - 1);
+                tempVal = result;
+                display.textContent = tempVal;
+            }
+            // functionality of delete button
+            else if (button.textContent == "Delete") {
+                tempVal = tempVal.substr(0, tempVal.length - 1);
+                display.textContent = tempVal;
+            }
+            // update the display with button press
+            else {
+                tempVal += button.textContent;
+                tempVal = tempVal.substring(0,numberSize); // set limit on number size
+                display.textContent = tempVal;
+                if (tempVal.length == numberSize) {
+                    tempVal = tempVal + "1"; // ensure max number does not have its last digit removed
+                };
+                // ensure proper functionaltiy when using result as the first number in calculation
+                if (tempVal == "+" || tempVal == "-" || tempVal == "×" || tempVal == "÷") {
+                    tempVal = result + "1"; // ensure the full result value remains
+                }; 
             };
-            if (tempVal == "+" || tempVal == "-" || tempVal == "×" || tempVal == "÷") {
-                tempVal = result + "1";
-            };  
-        }
+        };
+        // define operater and show it on the display
         if (button.textContent == "+" || button.textContent == "-" || button.textContent == "×" || button.textContent == "÷") {
             operation = button.textContent;
             val1 = tempVal.substring(0, tempVal.length - 1);
             tempVal = "";
             display.textContent = operation;
-        }
+        };
+        // grab the second value and perfrom calculation
         if (button.textContent == "=") {
             val2 = tempVal.substring(0, tempVal.length - 1);
             operate(operation, val1, val2);
             display.textContent = result;
             clearVariables();
-        }
-    })
-})
-
+        };
+    });
+});
 
 clearButton.addEventListener("click", function() {
     clearVariables();
@@ -62,30 +81,32 @@ clearButton.addEventListener("click", function() {
 function add(a,b) {
     a = parseFloat(a);
     b = parseFloat(b);
-    result = (a+b).toPrecision(precision);
-}
-
+    result = Math.round((a+b).toPrecision(precision) * 100000) / 100000;
+};
 function subtract(a,b) {
     a = parseFloat(a);
     b = parseFloat(b);
-    result = (a-b).toPrecision(precision);
-}
-
+    result = Math.round((a-b).toPrecision(precision) * 100000) / 100000;;
+};
 function multiply(a,b) {
     a = parseFloat(a);
     b = parseFloat(b);
     result = parseFloat((a*b).toPrecision(precision));
+    // convert large values to e notation
     if (result > 100000000) {
         result = parseFloat(result).toExponential();
-    }
-}
-
+    };
+};
 function divide(a,b) {
     a = parseFloat(a);
     b = parseFloat(b);
-    result = Math.round((a/b).toPrecision(precision) * 100000) / 100000;
-}
-
+    if (b === 0) {
+        result = "No No No";
+    }
+    else {
+        result = Math.round((a/b).toPrecision(precision) * 100000) / 100000;
+    };
+};
 function operate(operator,a,b) {
     switch(operator) {
         case "+":
@@ -96,13 +117,11 @@ function operate(operator,a,b) {
             return multiply(a,b)
         case "÷":
             return divide(a,b) 
-    }
-}
-
+    };
+};
 function clearVariables() {
     tempZero = "0";
-    tempVal2 = "";
     tempVal = "";
     val2 = "";
     operation = "";
-}
+};
